19f0bec7cf5ad9994f0ba38702911920b96a4542
IvanNH <nyhanitr@intranet.epfl.ch>
2016-06-07 17:43:43 +0200

    create cluster.m

--- cluster.m
+++ cluster.m
@@ -2,7 +2,6 @@ rng default; % For reproducibility
 X = [randn(100,2)*0.75+ones(100,2);
     randn(100,2)*0.5-ones(100,2)];
 
-figure;
 plot(X(:,1),X(:,2),'.');
 title 'Randomly Generated Data';
 
@@ -10,7 +9,6 @@ opts = statset('Display','final');
 [idx,C] = kmeans(X,2,'Distance','cityblock',...
     'Replicates',5,'Options',opts);
 
-figure;
 plot(X(idx==1,1),X(idx==1,2),'r.','MarkerSize',12)
 hold on
 plot(X(idx==2,1),X(idx==2,2),'b.','MarkerSize',12)
