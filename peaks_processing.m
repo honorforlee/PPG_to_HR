f8b4de662af5c30d36d9c35e5823d6a69545ca65
Ivan Ny Hanitra <Ivan Ny Hanitra>
2016-07-07 16:03:30 +0200

    clust_merge

--- peaks_processing.m
+++ peaks_processing.m
@@ -60,4 +60,4 @@ for k = 1:length(tx)                          % if minimum out of frame, take ne
     end
 end
 
-note_x = 0.2*note_1 + 0.2*note_2 + 0.6*delta;
+note_x = 0.1*note_1 + 0.2*note_2 + 0.7*delta;
