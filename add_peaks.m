9a70cb4a9e75504dac915a1b0fb4479103be1723
IvanNH <nyhanitr@intranet.epfl.ch>
2016-07-15 11:54:35 +0200

    update functions

--- add_peaks.m
+++ add_peaks.m
@@ -4,8 +4,7 @@
 function [kx_major, tx_major, sx_major, T] = add_peaksadd_peaks(t_,s_,td,d, kx_major,tx_major,sx_major, kx_add,tx_pos)
 if find(kx_add==0)                      % imaginary peak to create 
     zeros = find(kx_add==0);            % indexes where create a peak
-    tx_pos(isnan(tx_pos)) = [];         % remove nan values
-        
+            
     insert = @(a, x, n)cat(2,  x(1:n), a, x(n+1:end));      % insert(element inserted,array,position)
     
     for k = 1:length(zeros)
@@ -21,15 +20,16 @@ end
     kx_major(isnan(kx_major)) = [];             % remove NaN values
     kx_major = sort(kx_major);                  % sort
     
+    tx_pos(isnan(tx_pos)) = [];                 % remove nan values
     T_temp = mean(tx_pos);                      % peaks period (not considering missing peak)
     
-    for k = 1:length(kx_major)-1                % peak to add
-        if kx_major(k)~=kx_major(k+1)
+    for k = 1:length(kx_major)-1                
+        if kx_major(k)~=kx_major(k+1)           % peak to add
             tx_major(k+1) = td(kx_major(k+1)) + (td(kx_major(k+1)+1)-td(kx_major(k+1))) .* d(kx_major(k+1))./(d(kx_major(k+1))-d(kx_major(k+1)+1));      % linear interpolation of dhi and dho to get tx (@zero crossing)
         else                                    % peak to create
             tx_major(k+1) = tx_major(k) + T_temp;
         end
-        sx_major(k+1) = s_(kx_major(k+1)+1);        % local maxima
+        sx_major(k+1) = s_(kx_major(k+1)+1);        
     end
    
 T = mean(delta_tx(tx_major));
--- min_variance.m
+++ min_variance.m
@@ -251,7 +251,6 @@ if length(kx_major) >= 2
         % Search for missing peaks inside the frame
         [kx_add,tx_pos] = missing_peaks(kx,tx, kx_major,tx_major, tx_pos,T, note_x,NOTE_major);
         
-        
         % Add/create peak to major cluster
         [kx_major, tx_major, sx_major, T] = add_peaks(t_,s_,td,d, kx_major,tx_major,sx_major, kx_add,tx_pos);
         
@@ -262,18 +261,18 @@ if length(kx_major) >= 2
     % Miss first peak
     insert = @(a, x, n)cat(2,  x(1:n), a, x(n+1:end));      % insert(element inserted,array,position)
     
-    if abs( tx(1) - tx_major(1) ) > T + T/5
+    if similarity(T,abs( tx(1) - tx_major(1) ), 'relative') < 0.2 || abs( tx(1) - tx_major(1) ) > T + T/5             % 20% relative error or more than 20% error above T
         kx_major = insert(kx(1),kx_major,0);
-        tx_major = td(kx_major) + (td(kx_major+1)-td(kx_major)) .* d(kx_major)./(d(kx_major)-d(kx_major+1));      % linear interpolation of dhi and dho to get tx (@zero crossing)
-        sx_major = insert(sx_major(1),sx_major,0);                                                                % for visibility
+        tx_major = td(kx_major) + (td(kx_major+1)-td(kx_major)) .* d(kx_major)./(d(kx_major)-d(kx_major+1));          % linear interpolation of dhi and dho to get tx (@zero crossing)
+        sx_major = insert(sx_major(1),sx_major,0);                                                                    % for visibility
         T = mean(delta_tx(tx_major));
     end
     
     % Miss last peak
-    if abs( tx(end) - tx_major(end) ) > T + T/5
+    if similarity(T,abs( tx(end) - tx_major(end) ), 'relative') < 0.2  || abs( tx(end) - tx_major(end) ) > T + T/5    % 20% relative error or more than 20% error above T
         kx_major = insert(kx(end),kx_major,length(kx_major));
-        tx_major = td(kx_major) + (td(kx_major+1)-td(kx_major)) .* d(kx_major)./(d(kx_major)-d(kx_major+1));      % linear interpolation of dhi and dho to get tx (@zero crossing)
-        sx_major = insert(sx_major(end),sx_major,length(sx_major));                                               % for visibility
+        tx_major = td(kx_major) + (td(kx_major+1)-td(kx_major)) .* d(kx_major)./(d(kx_major)-d(kx_major+1));          % linear interpolation of dhi and dho to get tx (@zero crossing)
+        sx_major = insert(sx_major(end),sx_major,length(sx_major));                                                   % for visibility
         T = mean(delta_tx(tx_major));
     end
     
