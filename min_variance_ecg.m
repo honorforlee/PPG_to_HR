11a3434d27cb9fafed1f245f79fe5b61b313d174
Your Name <you@example.com>
2016-08-18 11:27:43 +0200

    complexity computation

---
+++ min_variance_ecg.m
@@ -0,0 +1,109 @@
+% Ivan NY HANITRA - Master thesis
+%       -- Clustering according to minimum variance of note_x  --
+
+function [kx_major,tx_major,sx_major, T, warning] = min_variance(kx,tx,sx, note_x, eps)
+kx_ = kx;
+
+%   - Clustering according to minimum variance of note_x -
+for i = 1:length(kx)
+    if kx_(i) ~= 0
+        
+        idx(1,i) = kx(i);
+        per(1,i) = tx(i);
+        note(1,i) = note_x(i);
+        j = 2;
+        
+        for k = i + 1 : length(kx)
+            
+            if  similarity(note_x(i),note_x(k),'variance') < eps;
+                
+                idx(j,i) = kx(k);
+                per(j,i) = tx(k);
+                note(j,i) = note_x(k);
+                
+                kx_(k) = 0;
+                j = j+1;
+            else
+                idx(j,i)=nan;
+                per(j,i)=nan;
+                note(j,i)=nan;
+                
+                j = j+1;
+            end
+            
+        end
+    end
+end
+
+%   - Remove columns -
+zero = find(~idx(1,:));
+
+for k = 1:length(zero)
+    idx(:,zero(k))=[];
+    per(:,zero(k))=[];
+    note(:,zero(k))=[];
+    zero = bsxfun(@minus ,zero,ones(1,length(zero))) ;
+end
+
+%   - Create cells: kx-tx-note_x -
+L = size(idx);
+for k = 1:L(2)
+    NAN_ = ~isnan(idx(:,k));         % extract non NAN value of idx, per, note
+    NAN_idx = idx(NAN_,k);
+    NAN_per = per(NAN_,k);
+    NAN_note = note(NAN_,k);
+    
+    idx_ = NAN_idx(find(NAN_idx));     % extract non zero value of idx, per, note
+    per_ = NAN_per(find(NAN_per));
+    note_ = NAN_note(find(NAN_note));
+    
+    clust_cell_temp{k,1} = idx_;            % kx
+    clust_cell_temp{k,2} = per_;            % tx
+    clust_cell_temp{k,3} = note_;           % note_x
+    
+    if clust_cell_temp{k,3} ~= 0            % case note_x = 0
+    NOTE_mean(k) = mean(clust_cell_temp{k,3});
+    else 
+        NOTE_mean(k)=0;
+    end    
+    
+    clear NAN_ NAN_idx NAN_per NAN_note idx_ per_ note_
+end
+
+% Sort clusters by NOTE
+NOTE = sort(NOTE_mean,'descend');                               % average of cluster note_x sorted
+
+for k = 1:L(2)
+    
+    clust_cell{k,1} = clust_cell_temp{NOTE_mean == NOTE(k),1};      % kx sorted
+    clust_cell{k,2} = clust_cell_temp{NOTE_mean == NOTE(k),2};      % tx sorted
+    clust_cell{k,3} = clust_cell_temp{NOTE_mean == NOTE(k),3};      % note_x sorted
+    
+end
+
+%   - Cluster notation: size, tx periodicity, note_x -
+for k = 1:L(2)
+    
+        SIZE(k) = length(clust_cell{k,1});                                 % size
+        [PER_T(k),PER_eps(k), PER_R(k)] = periodicity(clust_cell{k,2});    % tx periodicity
+        
+        if PER_eps(k) <= 0.1            % best periodicity note set to 0.1 otherwise increase too much the cluster note
+            PER_eps(k) = 0.1;
+        end
+        
+        clust_note(k) = (NOTE(k));          % cluster note EMPIRICAL
+                
+end
+
+tbl_note = table([1:L(2)]', SIZE', PER_T',PER_eps', PER_R', NOTE', clust_note','VariableNames',{'Cluster','Size','T','eps','R','Note_x','Cluster_note'});
+
+%   - Select major cluster -
+[major_clust major_idx] = max(clust_note);
+kx_major = clust_cell{major_idx,1}';
+idxs = arrayfun(@(x)find(kx==x,1),kx_major);
+tx_major = tx(idxs);
+sx_major = sx(idxs);
+T = mean(delta_tx(tx_major));
+warning = 0;
+
+
